408f1f2c5c854b8e2a29fe7a513962e27d06af68
Wesley Wichman <wesley@povo.co.nz>
2020-07-26 11:21:48 +1200

    Learnt how to add button
    Signed-off-by: Wesley Wichman <wesley@povo.co.nz>

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,43 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future main() async {
   await DotEnv().load('.env');
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.amber,
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text('Shopping'),
-          backgroundColor: Colors.blueGrey,
-        ),
-        body: Container(
-          child: Center( child: Text("test"),
-          ),
-        ),
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.amber,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Shopping'),
+        backgroundColor: Colors.blueGrey,
       ),
+      body: Container(child: LoginPage()),
     ),
-  );
+  ));
+}
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Center(
+              child: FlatButton(
+                onPressed: () {
+                  stderr.writeln('print me');
+                },
+                child: Text(
+                  "Flat Button",
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
 }
